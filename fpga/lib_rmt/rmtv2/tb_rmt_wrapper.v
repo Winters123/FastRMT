7575d0484d5721a8c76187f952e684f22fd1a133
18635723769@163.com <18635723769@163.com>
2020-10-29 03:26:56 +0800

    re-design parser for better timing

--- fpga/lib_rmt/rmtv2/tb_rmt_wrapper.v
+++ fpga/lib_rmt/rmtv2/tb_rmt_wrapper.v
@@ -61,7 +61,7 @@ initial begin
     s_axis_tvalid <= 1'b0;
     s_axis_tlast <= 1'b0;
     #CYCLE;
-    s_axis_tdata <= {64'hffffffffffffffff,64'hffffffffffffffff,159'b0, 8'h11, 72'b0, 16'h0008, 128'b0}; 
+    s_axis_tdata <= {64'hffffffffffffffff,64'hffffffff81000002,16'h0002,143'b0, 8'h11, 72'b0, 16'h0008, 128'hfffffffffffffff}; 
     s_axis_tkeep <= 64'hffffffffffffffff;
     s_axis_tuser <= 128'h0;
     s_axis_tvalid <= 1'b1;
