a73736301c9d182ce4241caf536cdc492fb11f93
Winters123 <18635723769@163.com>
2020-11-16 15:41:46 +0000

    move coe and mil files to rmtv2 directory

--- fpga/mqnic/AU250/fpga_100g/ip/rmt/blk_mem_gen_3.tcl
+++ fpga/mqnic/AU250/fpga_100g/ip/rmt/blk_mem_gen_3.tcl
@@ -3,7 +3,7 @@ create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_
 set_property -dict [list \
 	CONFIG.Memory_Type {Simple_Dual_Port_RAM} \
 	CONFIG.Load_Init_File {true} \
-	CONFIG.Coe_File {/../../../../../../rtl/key_mask.coe} \
+	CONFIG.Coe_File {/../../../../../../lib_rmt/rmtv2/key_mask.coe} \
 	CONFIG.Write_Depth_A {16} \
 	CONFIG.Write_Width_A {197} \
 	CONFIG.Read_Width_A {197} \
