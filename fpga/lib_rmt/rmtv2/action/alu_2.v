7263f8427a46fbf8c12dbd276e62609a91ac9801
wtao0221 <tao.wang0221@gmail.com>
2020-12-18 01:18:43 -0500

    add ctrl path in alu_2

--- fpga/lib_rmt/rmtv2/action/alu_2.v
+++ fpga/lib_rmt/rmtv2/action/alu_2.v
@@ -264,187 +264,425 @@ end
     checkme: CONTROL PATH
 */
 
-/****control path for 512b*****/
-wire [7:0]          mod_id; //module ID
-wire [15:0]         control_flag; //dst udp port num
-reg  [7:0]          c_index; //table index(addr)
-reg                 c_wr_en; //enable table write(wen)
+generate 
+	if (C_S_AXIS_DATA_WIDTH == 512) begin
+		/****control path for 512b*****/
+		wire [7:0]          mod_id; //module ID
+		wire [15:0]         control_flag; //dst udp port num
+		reg  [7:0]          c_index; //table index(addr)
+		reg                 c_wr_en; //enable table write(wen)
+		
+		reg  [2:0]          c_state;
+		
+		localparam IDLE_C = 0,
+		           WRITE_C = 1;
+		
+		assign mod_id = c_s_axis_tdata[368+:8];
+		assign control_flag = c_s_axis_tdata[335:320];
+		
+		//LE to BE switching
+		wire[C_S_AXIS_DATA_WIDTH-1:0] c_s_axis_tdata_swapped;
+		assign c_s_axis_tdata_swapped = {	c_s_axis_tdata[0+:8],
+											c_s_axis_tdata[8+:8],
+											c_s_axis_tdata[16+:8],
+											c_s_axis_tdata[24+:8],
+											c_s_axis_tdata[32+:8],
+											c_s_axis_tdata[40+:8],
+											c_s_axis_tdata[48+:8],
+											c_s_axis_tdata[56+:8],
+											c_s_axis_tdata[64+:8],
+											c_s_axis_tdata[72+:8],
+											c_s_axis_tdata[80+:8],
+											c_s_axis_tdata[88+:8],
+											c_s_axis_tdata[96+:8],
+											c_s_axis_tdata[104+:8],
+											c_s_axis_tdata[112+:8],
+											c_s_axis_tdata[120+:8],
+											c_s_axis_tdata[128+:8],
+											c_s_axis_tdata[136+:8],
+											c_s_axis_tdata[144+:8],
+											c_s_axis_tdata[152+:8],
+											c_s_axis_tdata[160+:8],
+											c_s_axis_tdata[168+:8],
+											c_s_axis_tdata[176+:8],
+											c_s_axis_tdata[184+:8],
+											c_s_axis_tdata[192+:8],
+											c_s_axis_tdata[200+:8],
+											c_s_axis_tdata[208+:8],
+											c_s_axis_tdata[216+:8],
+											c_s_axis_tdata[224+:8],
+											c_s_axis_tdata[232+:8],
+											c_s_axis_tdata[240+:8],
+											c_s_axis_tdata[248+:8],
+		                                    c_s_axis_tdata[256+:8],
+		                                    c_s_axis_tdata[264+:8],
+		                                    c_s_axis_tdata[272+:8],
+		                                    c_s_axis_tdata[280+:8],
+		                                    c_s_axis_tdata[288+:8],
+		                                    c_s_axis_tdata[296+:8],
+		                                    c_s_axis_tdata[304+:8],
+		                                    c_s_axis_tdata[312+:8],
+		                                    c_s_axis_tdata[320+:8],
+		                                    c_s_axis_tdata[328+:8],
+		                                    c_s_axis_tdata[336+:8],
+		                                    c_s_axis_tdata[344+:8],
+		                                    c_s_axis_tdata[352+:8],
+		                                    c_s_axis_tdata[360+:8],
+		                                    c_s_axis_tdata[368+:8],
+		                                    c_s_axis_tdata[376+:8],
+		                                    c_s_axis_tdata[384+:8],
+		                                    c_s_axis_tdata[392+:8],
+		                                    c_s_axis_tdata[400+:8],
+		                                    c_s_axis_tdata[408+:8],
+		                                    c_s_axis_tdata[416+:8],
+		                                    c_s_axis_tdata[424+:8],
+		                                    c_s_axis_tdata[432+:8],
+		                                    c_s_axis_tdata[440+:8],
+		                                    c_s_axis_tdata[448+:8],
+		                                    c_s_axis_tdata[456+:8],
+		                                    c_s_axis_tdata[464+:8],
+		                                    c_s_axis_tdata[472+:8],
+		                                    c_s_axis_tdata[480+:8],
+		                                    c_s_axis_tdata[488+:8],
+		                                    c_s_axis_tdata[496+:8],
+		                                    c_s_axis_tdata[504+:8]
+		                                };
+		
+		always @(posedge clk or negedge rst_n) begin
+		    if(~rst_n) begin
+		        c_wr_en <= 1'b0;
+		        c_index <= 4'b0;
+		
+		        c_m_axis_tdata <= 0;
+		        c_m_axis_tuser <= 0;
+		        c_m_axis_tkeep <= 0;
+		        c_m_axis_tvalid <= 0;
+		        c_m_axis_tlast <= 0;
+		
+		        c_state <= IDLE_C;
+		    end
+		    else begin
+		        case(c_state)
+		            IDLE_C: begin
+		                if(c_s_axis_tvalid && mod_id[7:3] == STAGE_ID && mod_id[2:0] == ACTION_ID && control_flag == 16'hf2f1)begin
+		                    c_wr_en <= 1'b1;
+		                    c_index <= c_s_axis_tdata[384+:8];
+		
+		                    c_m_axis_tdata <= 0;
+		                    c_m_axis_tuser <= 0;
+		                    c_m_axis_tkeep <= 0;
+		                    c_m_axis_tvalid <= 0;
+		                    c_m_axis_tlast <= 0;
+		
+		                    c_state <= WRITE_C;
+		
+		                end
+		                else begin
+		                    c_wr_en <= 1'b0;
+		                    c_index <= 4'b0; 
+		
+		                    c_m_axis_tdata <= c_s_axis_tdata;
+		                    c_m_axis_tuser <= c_s_axis_tuser;
+		                    c_m_axis_tkeep <= c_s_axis_tkeep;
+		                    c_m_axis_tvalid <= c_s_axis_tvalid;
+		                    c_m_axis_tlast <= c_s_axis_tlast;
+		
+		                    c_state <= IDLE_C;
+		                end
+		            end
+		            //support full table flush
+		            WRITE_C: begin
+		                if(c_s_axis_tlast && c_s_axis_tvalid) begin
+		                    c_wr_en <= 1'b0;
+		                    c_index <= 4'b0;
+		                    c_state <= IDLE_C;
+		                end
+		                else begin
+		                    if (c_s_axis_tvalid) begin
+		                        c_wr_en <= 1'b1;
+		                        c_index <= c_index + 4'b1;
+		                    end
+		                    c_state <= WRITE_C;
+		                end
+		            end
+		        endcase
+		
+		    end
+		end
+		
+		
+		//checkme: page table
+		page_tbl_16w_16d
+		page_tbl_16w_16d
+		(
+		    //write
+		    .addra(c_index),
+		    .clka(clk),
+		    .dina(c_s_axis_tdata_swapped[C_S_AXIS_DATA_WIDTH-1 -: 16]),
+		    .ena(1'b1),
+		    .wea(c_wr_en),
+		
+		    //match
+		    .addrb(vlan_id[7:4]),
+		    .clkb(clk),
+		    .doutb({addr_len,base_addr}),
+		    .enb(1'b1)
+		);
+		
+		//ram for key-value
+		//2 cycles to get value
+		blk_mem_gen_0
+		data_ram_32w_32d
+		(
+		    //store-related
+		    .addra(store_addr),
+		    .clka(clk),
+		    .dina(store_din),
+		    .ena(1'b1),
+		    .wea(store_en),
+		
+		    //load-related
+		    .addrb(load_addr),
+		    .clkb(clk),
+		    .doutb(load_data),
+		    .enb(1'b1)
+		);
+	end
+	else begin // control path for 256b
+		wire [7:0]          mod_id; //module ID
+		wire [15:0]         control_flag; //dst udp port num
+		wire[C_S_AXIS_DATA_WIDTH-1:0] c_s_axis_tdata_swapped;
+		assign c_s_axis_tdata_swapped = {	c_s_axis_tdata[0+:8],
+											c_s_axis_tdata[8+:8],
+											c_s_axis_tdata[16+:8],
+											c_s_axis_tdata[24+:8],
+											c_s_axis_tdata[32+:8],
+											c_s_axis_tdata[40+:8],
+											c_s_axis_tdata[48+:8],
+											c_s_axis_tdata[56+:8],
+											c_s_axis_tdata[64+:8],
+											c_s_axis_tdata[72+:8],
+											c_s_axis_tdata[80+:8],
+											c_s_axis_tdata[88+:8],
+											c_s_axis_tdata[96+:8],
+											c_s_axis_tdata[104+:8],
+											c_s_axis_tdata[112+:8],
+											c_s_axis_tdata[120+:8],
+											c_s_axis_tdata[128+:8],
+											c_s_axis_tdata[136+:8],
+											c_s_axis_tdata[144+:8],
+											c_s_axis_tdata[152+:8],
+											c_s_axis_tdata[160+:8],
+											c_s_axis_tdata[168+:8],
+											c_s_axis_tdata[176+:8],
+											c_s_axis_tdata[184+:8],
+											c_s_axis_tdata[192+:8],
+											c_s_axis_tdata[200+:8],
+											c_s_axis_tdata[208+:8],
+											c_s_axis_tdata[216+:8],
+											c_s_axis_tdata[224+:8],
+											c_s_axis_tdata[232+:8],
+											c_s_axis_tdata[240+:8],
+											c_s_axis_tdata[248+:8]};
 
-reg  [2:0]          c_state;
+        assign mod_id = c_s_axis_tdata[112+:8];
+        assign control_flag = c_s_axis_tdata[64+:16];
+		localparam	IDLE_C = 0,
+					PARSE_C = 1,
+					RAM_ENTRY = 2,
+					FLUSH_REST_C = 3;
+		// 
+		reg [2:0] c_state, c_state_next;
+		reg [C_S_AXIS_DATA_WIDTH-1:0]		r_tdata, c_s_axis_tdata_d1;
+		reg [C_S_AXIS_TUSER_WIDTH-1:0]		r_tuser, c_s_axis_tuser_d1;
+		reg [C_S_AXIS_DATA_WIDTH/8-1:0]		r_tkeep, c_s_axis_tkeep_d1;
+		reg									r_tlast, c_s_axis_tlast_d1;
+		reg									r_tvalid, c_s_axis_tvalid_d1;
 
-localparam IDLE_C = 1,
-           WRITE_C = 2;
+		reg [C_S_AXIS_DATA_WIDTH-1:0]		r_1st_tdata, r_1st_tdata_next;
+		reg [C_S_AXIS_TUSER_WIDTH-1:0]		r_1st_tuser, r_1st_tuser_next;
+		reg [C_S_AXIS_DATA_WIDTH/8-1:0]		r_1st_tkeep, r_1st_tkeep_next;
+		reg									r_1st_tlast, r_1st_tlast_next;
+		reg									r_1st_tvalid, r_1st_tvalid_next;
 
-assign mod_id = c_s_axis_tdata[368+:8];
-assign control_flag = c_s_axis_tdata[335:320];
+		reg [15:0]							c_wr_data_next, c_wr_data;
+		reg [7:0]							c_index_next, c_index;
+		reg									c_wr_en_next, c_wr_en;
 
-//LE to BE switching
-wire[C_S_AXIS_DATA_WIDTH-1:0] c_s_axis_tdata_swapped;
-assign c_s_axis_tdata_swapped = {	c_s_axis_tdata[0+:8],
-									c_s_axis_tdata[8+:8],
-									c_s_axis_tdata[16+:8],
-									c_s_axis_tdata[24+:8],
-									c_s_axis_tdata[32+:8],
-									c_s_axis_tdata[40+:8],
-									c_s_axis_tdata[48+:8],
-									c_s_axis_tdata[56+:8],
-									c_s_axis_tdata[64+:8],
-									c_s_axis_tdata[72+:8],
-									c_s_axis_tdata[80+:8],
-									c_s_axis_tdata[88+:8],
-									c_s_axis_tdata[96+:8],
-									c_s_axis_tdata[104+:8],
-									c_s_axis_tdata[112+:8],
-									c_s_axis_tdata[120+:8],
-									c_s_axis_tdata[128+:8],
-									c_s_axis_tdata[136+:8],
-									c_s_axis_tdata[144+:8],
-									c_s_axis_tdata[152+:8],
-									c_s_axis_tdata[160+:8],
-									c_s_axis_tdata[168+:8],
-									c_s_axis_tdata[176+:8],
-									c_s_axis_tdata[184+:8],
-									c_s_axis_tdata[192+:8],
-									c_s_axis_tdata[200+:8],
-									c_s_axis_tdata[208+:8],
-									c_s_axis_tdata[216+:8],
-									c_s_axis_tdata[224+:8],
-									c_s_axis_tdata[232+:8],
-									c_s_axis_tdata[240+:8],
-									c_s_axis_tdata[248+:8],
-                                    c_s_axis_tdata[256+:8],
-                                    c_s_axis_tdata[264+:8],
-                                    c_s_axis_tdata[272+:8],
-                                    c_s_axis_tdata[280+:8],
-                                    c_s_axis_tdata[288+:8],
-                                    c_s_axis_tdata[296+:8],
-                                    c_s_axis_tdata[304+:8],
-                                    c_s_axis_tdata[312+:8],
-                                    c_s_axis_tdata[320+:8],
-                                    c_s_axis_tdata[328+:8],
-                                    c_s_axis_tdata[336+:8],
-                                    c_s_axis_tdata[344+:8],
-                                    c_s_axis_tdata[352+:8],
-                                    c_s_axis_tdata[360+:8],
-                                    c_s_axis_tdata[368+:8],
-                                    c_s_axis_tdata[376+:8],
-                                    c_s_axis_tdata[384+:8],
-                                    c_s_axis_tdata[392+:8],
-                                    c_s_axis_tdata[400+:8],
-                                    c_s_axis_tdata[408+:8],
-                                    c_s_axis_tdata[416+:8],
-                                    c_s_axis_tdata[424+:8],
-                                    c_s_axis_tdata[432+:8],
-                                    c_s_axis_tdata[440+:8],
-                                    c_s_axis_tdata[448+:8],
-                                    c_s_axis_tdata[456+:8],
-                                    c_s_axis_tdata[464+:8],
-                                    c_s_axis_tdata[472+:8],
-                                    c_s_axis_tdata[480+:8],
-                                    c_s_axis_tdata[488+:8],
-                                    c_s_axis_tdata[496+:8],
-                                    c_s_axis_tdata[504+:8]
-                                };
+		always @(*) begin
+			c_state_next = c_state;
 
-always @(posedge clk or negedge rst_n) begin
-    if(~rst_n) begin
-        c_wr_en <= 1'b0;
-        c_index <= 4'b0;
+			r_tdata = 0;
+			r_tkeep = 0;
+			r_tuser = 0;
+			r_tlast = 0;
+			r_tvalid = 0;
 
-        c_m_axis_tdata <= 0;
-        c_m_axis_tuser <= 0;
-        c_m_axis_tkeep <= 0;
-        c_m_axis_tvalid <= 0;
-        c_m_axis_tlast <= 0;
+			r_1st_tdata_next = r_1st_tdata;
+			r_1st_tkeep_next = r_1st_tkeep;
+			r_1st_tuser_next = r_1st_tuser;
+			r_1st_tlast_next = r_1st_tlast;
+			r_1st_tvalid_next = r_1st_tvalid;
 
-        c_state <= IDLE_C;
-    end
-    else begin
-        case(c_state)
-            IDLE_C: begin
-                if(c_s_axis_tvalid && mod_id[7:3] == STAGE_ID && mod_id[2:0] == ACTION_ID && control_flag == 16'hf2f1)begin
-                    c_wr_en <= 1'b1;
-                    c_index <= c_s_axis_tdata[384+:8];
+			c_index_next = c_index;
+			c_wr_en_next = 0;
+			c_wr_data_next = c_wr_data;
 
-                    c_m_axis_tdata <= 0;
-                    c_m_axis_tuser <= 0;
-                    c_m_axis_tkeep <= 0;
-                    c_m_axis_tvalid <= 0;
-                    c_m_axis_tlast <= 0;
+			case (c_state) 
+				IDLE_C: begin // 1st segment
+					r_tvalid = 0;
+					if (c_s_axis_tvalid) begin
+						// store 1st segment
+						r_1st_tdata_next = c_s_axis_tdata;
+						r_1st_tuser_next = c_s_axis_tuser;
+						r_1st_tkeep_next = c_s_axis_tkeep;
+						r_1st_tlast_next = c_s_axis_tlast;
+						r_1st_tvalid_next = c_s_axis_tvalid;
 
-                    c_state <= WRITE_C;
+						c_state_next = PARSE_C;
+					end
+				end
+				PARSE_C: begin // 2nd segment
+					if (mod_id[7:3] == STAGE_ID && mod_id[2:0] == ACTION_ID && 
+						control_flag == 16'hf2f1 && c_s_axis_tvalid) begin
+						// should not emit segment
+						c_index_next = c_s_axis_tdata[128+:8];
+						c_state_next = RAM_ENTRY;
+					end
+					else if (!c_s_axis_tvalid) begin
+					end
+					else begin
+						// emit
+						r_tdata = r_1st_tdata;
+						r_tkeep = r_1st_tkeep;
+						r_tuser = r_1st_tuser;
+						r_tlast = r_1st_tlast;
+						r_tvalid = r_1st_tvalid;
+						c_state_next = FLUSH_REST_C;
+					end
+				end
+				RAM_ENTRY: begin // 3rd segment
+					if (c_s_axis_tvalid) begin
+						c_wr_en_next = 1; // next clk to write
+						c_wr_data_next = c_s_axis_tdata_swapped[255-:16];
+						
+						c_state_next = FLUSH_REST_C;
+					end
+				end
+				FLUSH_REST_C: begin
+					c_wr_en_next = 0;
+					r_tdata = c_s_axis_tdata_d1;
+					r_tkeep = c_s_axis_tkeep_d1;
+					r_tuser = c_s_axis_tuser_d1;
+					r_tlast = c_s_axis_tlast_d1;
+					r_tvalid = c_s_axis_tvalid_d1;
+					if (c_s_axis_tvalid_d1 && c_s_axis_tlast_d1) begin
+						c_state_next = IDLE_C;
+					end
+				end
+			endcase
+		end
 
-                end
-                else begin
-                    c_wr_en <= 1'b0;
-                    c_index <= 4'b0; 
+		always @(posedge clk) begin
+			if (~rst_n) begin
+				c_state <= IDLE_C;
 
-                    c_m_axis_tdata <= c_s_axis_tdata;
-                    c_m_axis_tuser <= c_s_axis_tuser;
-                    c_m_axis_tkeep <= c_s_axis_tkeep;
-                    c_m_axis_tvalid <= c_s_axis_tvalid;
-                    c_m_axis_tlast <= c_s_axis_tlast;
-
-                    c_state <= IDLE_C;
-                end
-            end
-            //support full table flush
-            WRITE_C: begin
-                if(c_s_axis_tlast && c_s_axis_tvalid) begin
-                    c_wr_en <= 1'b0;
-                    c_index <= 4'b0;
-                    c_state <= IDLE_C;
-                end
-                else begin
-                    if (c_s_axis_tvalid) begin
-                        c_wr_en <= 1'b1;
-                        c_index <= c_index + 4'b1;
-                    end
-                    c_state <= WRITE_C;
-                end
-            end
-        endcase
-
-    end
-end
+				// control output
+				c_m_axis_tdata <= 0;
+				c_m_axis_tuser <= 0;
+				c_m_axis_tkeep <= 0;
+				c_m_axis_tlast <= 0;
+				c_m_axis_tvalid <= 0;
+				//
+				c_index <= 0;
+				c_wr_en <= 0;
+				c_wr_data <= 0;
+			end
+			else begin
+				c_state <= c_state_next;
 
 
-//checkme: page table
-page_tbl_16w_16d
-page_tbl_16w_16d
-(
-    //write
-    .addra(c_index),
-    .clka(clk),
-    .dina(c_s_axis_tdata_swapped[C_S_AXIS_DATA_WIDTH-1 -: 16]),
-    .ena(1'b1),
-    .wea(c_wr_en),
+				// output ctrl master signals
+				c_m_axis_tdata <= r_tdata;
+				c_m_axis_tkeep <= r_tkeep;
+				c_m_axis_tuser <= r_tuser;
+				c_m_axis_tlast <= r_tlast;
+				c_m_axis_tvalid <= r_tvalid;
+				//
+				c_index <= c_index_next;
+				c_wr_en <= c_wr_en_next;
+				c_wr_data <= c_wr_data_next;
+			end
+		end
 
-    //match
-    .addrb(vlan_id[7:4]),
-    .clkb(clk),
-    .doutb({addr_len,base_addr}),
-    .enb(1'b1)
-);
-
-//ram for key-value
-//2 cycles to get value
-blk_mem_gen_0
-data_ram_32w_32d
-(
-    //store-related
-    .addra(store_addr),
-    .clka(clk),
-    .dina(store_din),
-    .ena(1'b1),
-    .wea(store_en),
-
-    //load-related
-    .addrb(load_addr),
-    .clkb(clk),
-    .doutb(load_data),
-    .enb(1'b1)
-);
+		always @(posedge clk) begin
+			if (~rst_n) begin
+				// delayed 1 clk
+				c_s_axis_tdata_d1 <= 0;
+				c_s_axis_tuser_d1 <= 0;
+				c_s_axis_tkeep_d1 <= 0;
+				c_s_axis_tlast_d1 <= 0;
+				c_s_axis_tvalid_d1 <= 0;
+				//
+				r_1st_tdata <= 0;
+				r_1st_tkeep <= 0;
+				r_1st_tuser <= 0;
+				r_1st_tlast <= 0;
+				r_1st_tvalid <= 0;
+			end
+			else begin
+				// delayed 1 clk
+				c_s_axis_tdata_d1 <= c_s_axis_tdata;
+				c_s_axis_tuser_d1 <= c_s_axis_tuser;
+				c_s_axis_tkeep_d1 <= c_s_axis_tkeep;
+				c_s_axis_tlast_d1 <= c_s_axis_tlast;
+				c_s_axis_tvalid_d1 <= c_s_axis_tvalid;
+				// 
+				r_1st_tdata <= r_1st_tdata_next;
+				r_1st_tkeep <= r_1st_tkeep_next;
+				r_1st_tuser <= r_1st_tuser_next;
+				r_1st_tlast <= r_1st_tlast_next;
+				r_1st_tvalid <= r_1st_tvalid_next;
+			end
+		end
+		//checkme: page table
+		page_tbl_16w_16d
+		page_tbl_16w_16d
+		(
+		    //write
+		    .addra(c_index),
+		    .clka(clk),
+		    .dina(c_wr_data),
+		    .ena(1'b1),
+		    .wea(c_wr_en),
+		
+		    //match
+		    .addrb(vlan_id[7:4]),
+		    .clkb(clk),
+		    .doutb({addr_len,base_addr}),
+		    .enb(1'b1)
+		);
+		
+		//ram for key-value
+		//2 cycles to get value
+		blk_mem_gen_0
+		data_ram_32w_32d
+		(
+		    //store-related
+		    .addra(store_addr),
+		    .clka(clk),
+		    .dina(store_din),
+		    .ena(1'b1),
+		    .wea(store_en),
+		
+		    //load-related
+		    .addrb(load_addr),
+		    .clkb(clk),
+		    .doutb(load_data),
+		    .enb(1'b1)
+		);
+	end
+endgenerate
 
 
 endmodule
