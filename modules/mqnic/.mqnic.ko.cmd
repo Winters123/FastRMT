95faff3bdf827d898cdf8dad9d0479f12a387671
root <18635723769@163.com>
2020-12-28 23:55:05 -0500

    del mqnic driver

--- modules/mqnic/.mqnic.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/earl-07/corundum-forked/modules/mqnic/mqnic.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/earl-07/corundum-forked/modules/mqnic/mqnic.ko /home/earl-07/corundum-forked/modules/mqnic/mqnic.o /home/earl-07/corundum-forked/modules/mqnic/mqnic.mod.o;  true
