5309d18a1b160b08df53886a4dc754b67b0c757d
18635723769@163.com <18635723769@163.com>
2021-01-18 15:10:01 +0800

    check cam and timing

--- fpga/lib_rmt/rmtv2/lookup/lookup_engine.v
+++ fpga/lib_rmt/rmtv2/lookup/lookup_engine.v
@@ -435,12 +435,7 @@ generate
 				.BUSY				(),
         	    .MATCH				(match),
         	    .MATCH_ADDR			(match_addr[3:0]),
-
-        	    //.WE				(lookup_din_en),
-        	    //.WR_ADDR			(lookup_din_addr),
-        	    //.DATA_MASK		(lookup_din_mask),  
-        	    //.DIN				(lookup_din),
-
+				
         	    .WE                 (c_wr_en_cam),
         	    .WR_ADDR            (c_index_cam[3:0]),
         	    .DATA_MASK          (),  //TODO do we need ternary matching?
@@ -466,11 +461,6 @@ generate
         	    .BUSY				(),
         	    .MATCH				(match),
         	    .MATCH_ADDR			(match_addr[3:0]),
-
-        	    //.WE				(lookup_din_en),
-        	    //.WR_ADDR			(lookup_din_addr),
-        	    //.DATA_MASK		(lookup_din_mask),  
-        	    //.DIN				(lookup_din),
 
         	    .WE                 (c_wr_en_cam),
         	    .WR_ADDR            (c_index_cam[3:0]),
@@ -501,33 +491,6 @@ generate
             .enb(1'b1)
         );
 
-        //(*mark_debug = "true"*) wire        c_wr_en_cam_dbg;
-        // (*mark_debug = "true"*) wire        c_wr_en_act_dbg;
-        // (*mark_debug = "true"*) wire [3:0]  c_index_act_dbg;
-        //(* mark_debug= "true"*) wire [3:0]  c_index_cam_dbg;
-        //(* mark_debug= "true"*) wire [15:0] cam_entry_reg_dbg;
-        //(* mark_debug= "true"*) wire [24:0] dbg_action_in;
-        
-        // (* mark_debug= "true"*) wire [24:0]   dbg_action_out;
-        // (* mark_debug= "true"*) wire          dbg_action_out_valid;
-        
-        // assign c_wr_en_act_dbg = c_wr_en_act;
-        //assign c_wr_en_cam_dbg = c_wr_en_cam;
-        // assign c_index_act_dbg = c_index_act;
-        //assign c_index_cam_dbg = c_index_cam;
-        //assign cam_entry_reg_dbg = cam_entry_reg[36 -: 16];
-        //assign dbg_action_in = act_entry_tmp[275 +: 25];
-        
-
-        // (*mark_debug = "true"*) wire            match_dbg;
-        // (*mark_debug = "true"*) wire [3:0]      match_addr_dbg;
-        // (*mark_debug = "true"*) wire [3:0]      vlan_id_dbg;
-
-        // assign match_dbg = match;
-        // assign match_addr_dbg = match_addr;
-        // assign vlan_id_dbg = vlan_id[7:4];
-        // assign dbg_action_out_valid= action_valid;
-        // assign dbg_action_out = action_wire[275 +: 25];
 
 
     end
